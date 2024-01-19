b3a0b83ec80db3dd7f086ced4bad3e6c8328c7db
Maek0s <maek0szaho@gmail.com>
2024-01-19 19:53:27 +0100

    +

---
+++ Ejers 100 - 199/p192_WA.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class p192_WA {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = -1;
+
+        while (n != 0) {
+            n = s.nextInt();
+
+            if (n == 0) { break;}
+            while (n != 1) {
+                if (n % 3 == 0) {
+                    n = n / 3;
+                    if (n > 5) {
+                        n -= 5;
+                    }
+                } else if (n > 5) {
+                    n -= 5;
+                } else {
+                    System.out.println("NO");
+                    break;
+                }
+            }
+            if (n == 1) { System.out.println("SI"); }
+        }
+    }
+}
