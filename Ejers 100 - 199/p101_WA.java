41cf2ee86d389eb7325c13ebd3b291f012b05905
Maek0s <maek0szaho@gmail.com>
2024-01-24 19:57:26 +0100

    +

--- Ejers 100 - 199/p101_WA.java
+++ Ejers 100 - 199/p101_WA.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
  * 1 63 62 4 5 59 58 8 56 10 11 53 52 14 15 49 48 18 19 45 44 22 23 41 25 39 38 28 29 35 34 32 33 31 30 36 37 27 26 40 24 42 43 21 20 46 47 17 16 50 51 13 12 54 55 9 57 7 6 60 61 3 2 64
  */
 
+ // El problema a solucionar creo que es que el diabolico no cumple la norma numero 2 y no esta comprobada
+ // SOL: Se puede sacar si es diabolico sumando y tal y luego el esoterico haciendo norma a norma, para calculos se pueden llegar a crear nuevos modulos.
+
 public class p101_WA {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -66,7 +69,7 @@ public class p101_WA {
                             sumaextra = (matriz[0][(c - 1) / 2] + matriz[0][(c - 1) / 2 + 1] + matriz[(c - 1) / 2][0] + matriz[(c - 1) / 2 + 1][0] + matriz[(c - 1)][(c - 1) / 2] + matriz[(c - 1)][(c - 1) / 2 + 1] + matriz[(c - 1) / 2][c - 1] + matriz[(c - 1) / 2 + 1][c - 1]) / 2; // Centro lados
                             if (suma == sumaextra) {
                                 // System.out.println("Centro lados pasado");
-                                resultado = "ESOTERICO";
+                                resultado = "ESOTERICO"; // ¡COMPROBAR!
                             }
                         }
                     }
@@ -88,13 +91,13 @@ public class p101_WA {
                 }
             }
 
-            // System.out.println();
-            // for (int i = 0; i < c; i++) {
-            //     for (int j = 0; j < c; j++) {
-            //        System.out.print(matriz[i][j]+" ");
-            //     }
-            //     System.out.println();
-            // }
+            System.out.println();
+            for (int i = 0; i < c; i++) {
+                for (int j = 0; j < c; j++) {
+                  System.out.print(matriz[i][j]+" ");
+                }
+                System.out.println();
+            }
         
             System.out.println(resultado);
         }
--- Ejers 100 - 199/p192_WA.java
+++ Ejers 100 - 199/p192_WA.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
+// El problema es cuando va por un sitio y ya no le sirve la otra ruta, será fixeable si se puede tirar para atras en caso de que vaya a tirar error
+
 public class p192_WA {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         int n = -1;
+        // boolean fix = false;
 
         while (n != 0) {
             n = s.nextInt();
@@ -13,19 +16,18 @@ public class p192_WA {
             while (n != 1) {
                 if (n % 3 == 0) {
                     n = n / 3;
-                    if (n > 5) {
-                        n -= 5;
-                    }
                 } else if (n > 5) {
                     n -= 5;
                 } else {
-                    System.out.println("NO");
                     break;
                 }
             }
-            if (n == 1) { System.out.println("SI"); }
-
-            s.close();
+            if (n == 1) {
+                System.out.println("SI"); }
+            else {
+                System.out.println("NO");
+            }
         }
+        s.close();
     }
 }
