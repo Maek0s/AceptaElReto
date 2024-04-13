e60bfb8329b3b58079cfac61bb1a314240327532
Maek0s <maek0szaho@gmail.com>
2024-04-13 21:00:25 +0200

    1 ejercicio pendiente, 1 RTE

---
+++ Ejers 100 - 199/p102_PD.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class p102_PD {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        boolean fin = true;
+
+        while (fin) {
+            String entrada = s.nextLine();
+            System.out.println(codificar(entrada));
+            
+        }
+
+        s.close();
+    }
+
+    public static String codificar (String entrada) {
+        String resultado = "";
+
+        for (int i = 0; i < entrada.length(); i++) {
+            //resultado = Character.toString(entrada.charAt(i)) + 1;
+            char letra = (char) (entrada.charAt(i) + 1);
+            resultado += letra;
+        }
+
+        return resultado;
+    }
+}
