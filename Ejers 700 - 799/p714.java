fcdc9dd37a8cf49ca20003054fc301fac7933578
Maek0s <145163254+Maek0s@users.noreply.github.com>
2024-04-16 02:49:37 +0200

    Update p714.java

--- Ejers 700 - 799/p714.java
+++ Ejers 700 - 799/p714.java
@@ -5,17 +5,49 @@ public class p714 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        ArrayList <String> equipos = new ArrayList<String>();
+        int c = -1;
 
-        String c = "";
+        while (c != 0) {
+            ArrayList <String> equipos = new ArrayList <String>();
+            ArrayList <Integer> goles = new ArrayList<Integer>();
+            
+            int contequipos = 0;
+            int contgoles = 0;
 
-        while (!c.equals("0")) {
-            c = s.nextLine();
+            c = s.nextInt();
+            s.nextLine();
 
-            for (int i = 0; i < Integer.parseInt(c); i++) {
-                equipos.add(c);
+            if (c == 0) { break; }
+
+            for (int i = 0; i < c; i++) {
+                equipos.add(s.next());
             }
+
+            for (int i = 0; i < (c * 2 - 2); i++) {
+                goles.add(s.nextInt());
+            }
+
+            while (equipos.size() != 1) {
+                if (goles.get(0) > goles.get(1)) {
+                    equipos.remove(contequipos + 1);
+                } else {
+                    equipos.remove(contequipos);
+                }
+
+                goles.remove(0);
+                if (goles.size() > 0) {
+                    goles.remove(0);
+                }
+                contequipos++;
+                contgoles++;
+                if (contgoles >= equipos.size()) {
+                    contgoles = 0;
+                    contequipos = 0;
+                }
+            }
+            System.out.println(equipos.get(0));
         }
-        
+
+        s.close(); // Close del Scanner
     }
 }
